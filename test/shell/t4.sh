ff838700d0f38d54b61bad2a848b83d92787b2dd
James Shubin <james@shubin.ca>
2016-02-02 04:36:12 -0500

    Add a fan in example and test

---
+++ test/shell/t4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+. etcd.sh	# start etcd as job # 1
+
+# should take slightly more than 25s, but fail if we take 35s)
+timeout --kill-after=35s 30s ./mgmt run --file t4.yaml --converged-timeout=5 --no-watch &
+
+#jobs	# etcd is 1
+#wait -n 2	# wait for mgmt to exit
+. wait.sh	# wait for everything except etcd

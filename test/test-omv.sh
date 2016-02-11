89182521de8e30badba3e7cbbff2955b830d6608
James Shubin <james@shubin.ca>
2016-02-10 19:00:45 -0500

    Fix up path issues with vtest+

--- test/test-omv.sh
+++ test/test-omv.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -ie
 # simple test harness for testing mgmt via omv
+CWD=`pwd`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"	# dir!
+cd "$DIR" >/dev/null	# work from test directory
+
+# vtest+ tests
 vtest+ omv/helloworld.yaml
+
+# return to original dir
+cd "$CWD" >/dev/null

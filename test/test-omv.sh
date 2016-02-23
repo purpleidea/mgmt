a56fb3c8cdd1fb2ad6735595ee90fa9a6e24e12b
James Shubin <james@shubin.ca>
2016-02-22 20:00:15 -0500

    Update items for Jenkins tests

--- test/test-omv.sh
+++ test/test-omv.sh
@@ -6,10 +6,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"	# dir!
 cd "$DIR" >/dev/null	# work from test directory
 
 # vtest+ tests
+RET=0
 for i in omv/*.yaml; do
 	echo "running: vtest+ $i"
 	vtest+ "$i"
+	if [ $? -ne 0 ]; then
+		RET=1
+		break	# remove this if we should run all tests even if one fails
+	fi
 done
 
 # return to original dir
 cd "$CWD" >/dev/null
+exit $RET

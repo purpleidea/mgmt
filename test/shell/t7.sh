c1605a4f225136f149f1fb05338501fd098a50f5
James Shubin <james@shubin.ca>
2016-07-05 07:20:21 -0400

    Add test case for urfave regression
    Credit to jerith for helping me hack this together :)

---
+++ test/shell/t7.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+# run empty graph
+timeout --kill-after=20s 15s ./mgmt run &
+pid=$!
+sleep 5s	# let it converge
+$(sleep 3s && killall -SIGINT mgmt)&	# send ^C to exit mgmt
+wait $pid	# get exit status
+exit $?

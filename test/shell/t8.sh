1cf88d95404f27b033645ae568c59b681cf593f3
James Shubin <james@shubin.ca>
2016-09-02 02:27:39 -0400

    test: Increase timeouts of t8
    Increase the timeouts in the rare chance that this is slow performing
    travis, and not just an etcd regression.

--- test/shell/t8.sh
+++ test/shell/t8.sh
@@ -1,22 +1,22 @@
 #!/bin/bash -e
 
 # run empty graphs, we're just testing etcd clustering
-timeout --kill-after=120s 90s ./mgmt run --hostname h1 --tmp-prefix &
+timeout --kill-after=180s 120s ./mgmt run --hostname h1 --tmp-prefix &
 pid1=$!
-sleep 10s	# let it startup
+sleep 15s	# let it startup
 
-timeout --kill-after=120s 90s ./mgmt run --hostname h2 --seeds http://127.0.0.1:2379 --client-urls http://127.0.0.1:2381 --server-urls http://127.0.0.1:2382 --tmp-prefix &
+timeout --kill-after=180s 120s ./mgmt run --hostname h2 --seeds http://127.0.0.1:2379 --client-urls http://127.0.0.1:2381 --server-urls http://127.0.0.1:2382 --tmp-prefix &
 pid2=$!
-sleep 10s
+sleep 15s
 
-$(sleep 10s && kill -SIGINT $pid2)&	# send ^C to exit 2nd mgmt
+$(sleep 15s && kill -SIGINT $pid2)&	# send ^C to exit 2nd mgmt
 wait $pid2
 e=$?
 if [ $e -ne 0 ]; then
 	exit $e
 fi
 
-$(sleep 10s && kill -SIGINT $pid1)&	# send ^C to exit 1st mgmt
+$(sleep 15s && kill -SIGINT $pid1)&	# send ^C to exit 1st mgmt
 wait $pid1	# get exit status
 # if pid1 exits because of a timeout, then it blocked, and this is a bug!
 exit $?

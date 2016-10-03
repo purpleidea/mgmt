cd26a0770d26ee8a812c1cf989270779c06984d0
James Shubin <james@shubin.ca>
2016-10-03 14:50:28 -0400

    test: Catch go vet issues in subpackages
    Improve our tests now that we have multiple packages to test.

--- test/test-govet.sh
+++ test/test-govet.sh
@@ -4,6 +4,8 @@ echo running test-govet.sh
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"	# dir!
 cd "${ROOT}"
 
-go vet && echo PASS || exit 1	# since it doesn't output an ok message on pass
-grep 'log.' *.go | grep '\\n"' && echo 'no \n needed in log.Printf()' && exit 1 || echo PASS	# no \n needed in log.Printf()
-grep 'case _ = <-' *.go && echo 'case _ = <- can be simplified to: case <-' && exit 1 || echo PASS	# this can be simplified
+for file in `find . -maxdepth 3 -type f -name '*.go' -not -path './old/*' -not -path './tmp/*'`; do
+	go vet "$file" && echo PASS || exit 1	# since it doesn't output an ok message on pass
+	grep 'log.' "$file" | grep '\\n"' && echo 'no \n needed in log.Printf()' && exit 1 || echo PASS	# no \n needed in log.Printf()
+	grep 'case _ = <-' "$file" && echo 'case _ = <- can be simplified to: case <-' && exit 1 || echo PASS	# this can be simplified
+done

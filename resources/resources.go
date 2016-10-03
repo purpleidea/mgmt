f219c2649d25aae9455620c59b4ae5e4158781bc
James Shubin <james@shubin.ca>
2016-10-03 15:26:41 -0400

    godoc: Document resources package

--- resources/resources.go
+++ resources/resources.go
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+// Package resources provides the resource framework and idempotent primitives.
 package resources
 
 import (

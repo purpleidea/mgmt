da7204052c612e16984ca2e1260597b461d81ba9
Dave Kempe <dave@sol1.com.au>
2017-11-25 00:39:02 +1100

    misc: Fixed make-deps error handling
    - Added some error handling to deal with broken systems
    - Updated requirement to golang 1.8
    - Made gometalinter.v1 optional

--- misc/make-deps.sh
+++ misc/make-deps.sh
@@ -4,6 +4,13 @@ XPWD=`pwd`
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"	# dir!
 cd "${ROOT}" >/dev/null
 
+function errorhandler () {
+
+echo "Something went wrong! Hey, on the bright side if it breaks, you can keep both halves!"
+exit
+
+}
+
 travis=0
 if env | grep -q '^TRAVIS=true$'; then
 	travis=1
@@ -33,13 +40,13 @@ if [ ! -z "$YUM" ]; then
 
 fi
 if [ ! -z "$APT" ]; then
-	$sudo_command $APT install -y libvirt-dev || true
-	$sudo_command $APT install -y libaugeas-dev || true
-	$sudo_command $APT install -y libpcap0.8-dev || true
+	$sudo_command $APT install -y libvirt-dev || errorhandler
+	$sudo_command $APT install -y libaugeas-dev || errorhandler
+	$sudo_command $APT install -y libpcap0.8-dev || errorhandler
 fi
 
 if [ ! -z "$BREW" ]; then
-	$BREW install libvirt || true
+	$BREW install libvirt || errorhandler
 fi
 
 if [ ! -z "$PACMAN" ]; then
@@ -56,8 +63,8 @@ if [ $travis -eq 0 ]; then
 		$sudo_command $APT update
 		$sudo_command $APT install -y golang make gcc packagekit mercurial
 		# one of these two golang tools packages should work on debian
-		$sudo_command $APT install -y golang-golang-x-tools || true
-		$sudo_command $APT install -y golang-go.tools || true
+		$sudo_command $APT install -y golang-golang-x-tools || errorhandler
+		$sudo_command $APT install -y golang-go.tools || errorhandler
 	fi
 	if [ ! -z "$PACMAN" ]; then
 		$sudo_command $PACMAN -S --noconfirm go
@@ -65,8 +72,8 @@ if [ $travis -eq 0 ]; then
 fi
 
 # if golang is too old, we don't want to fail with an obscure error later
-if go version | grep 'go1\.[012345]\.'; then
-	echo "mgmt requires go1.6 or higher."
+if go version | grep 'go1\.[01234567]\.'; then
+	echo "mgmt requires go1.8 or higher."
 	exit 1
 fi
 
@@ -80,5 +87,12 @@ if [[ $ret != 0 ]]; then
 fi
 go get golang.org/x/tools/cmd/stringer	# for automatic stringer-ing
 go get github.com/golang/lint/golint	# for `golint`-ing
+
+which gometalinter.v1 1> /dev/null 2>&1
+ret=$?
+echo $ret
+if [[ $ret = 0 ]]; then
 go get -u gopkg.in/alecthomas/gometalinter.v1 && mv "$(dirname $(which gometalinter.v1))/gometalinter.v1" "$(dirname $(which gometalinter.v1))/gometalinter" && gometalinter --install	# bonus
+else echo "Unable to install gometalinter.v1, it isn't too important, but nice to have"
+fi
 cd "$XPWD" >/dev/null

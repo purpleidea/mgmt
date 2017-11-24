09c20f4c3259eb456540e7012644a3dfe1237f10
Dave Kempe <dave@sol1.com.au>
2017-11-25 00:39:02 +1100

    misc: Fixed make-deps error handling
    - Added some error handling to deal with broken systems
    - Updated requirement to golang 1.8
    - Handle default GOPATH for gometalinter

--- misc/make-deps.sh
+++ misc/make-deps.sh
@@ -4,6 +4,11 @@ XPWD=`pwd`
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"	# dir!
 cd "${ROOT}" >/dev/null
 
+function errorhandler () {
+	echo "Something went wrong! Hey, on the bright side if it breaks, you can keep both halves!"
+	exit
+}
+
 travis=0
 if env | grep -q '^TRAVIS=true$'; then
 	travis=1
@@ -28,45 +33,45 @@ if [ -z "$YUM" -a -z "$APT" -a -z "$BREW" -a -z "$PACMAN" ]; then
 fi
 
 if [ ! -z "$YUM" ]; then
-	$sudo_command $YUM install -y libvirt-devel
-	$sudo_command $YUM install -y augeas-devel
+	$sudo_command $YUM install -y libvirt-devel || errorhandler
+	$sudo_command $YUM install -y augeas-devel || errorhandler
 
 fi
 if [ ! -z "$APT" ]; then
-	$sudo_command $APT install -y libvirt-dev || true
-	$sudo_command $APT install -y libaugeas-dev || true
-	$sudo_command $APT install -y libpcap0.8-dev || true
+	$sudo_command $APT install -y libvirt-dev || errorhandler
+	$sudo_command $APT install -y libaugeas-dev || errorhandler
+	$sudo_command $APT install -y libpcap0.8-dev || errorhandler
 fi
 
 if [ ! -z "$BREW" ]; then
-	$BREW install libvirt || true
+	$BREW install libvirt || errorhandler
 fi
 
 if [ ! -z "$PACMAN" ]; then
-	$sudo_command $PACMAN -S --noconfirm libvirt augeas libpcap
+	$sudo_command $PACMAN -S --noconfirm libvirt augeas libpcap || errorhandler
 fi
 
 if [ $travis -eq 0 ]; then
 	if [ ! -z "$YUM" ]; then
 		# some go dependencies are stored in mercurial
-		$sudo_command $YUM install -y golang golang-googlecode-tools-stringer hg
+		$sudo_command $YUM install -y golang golang-googlecode-tools-stringer hg || errorhandler
 
 	fi
 	if [ ! -z "$APT" ]; then
 		$sudo_command $APT update
-		$sudo_command $APT install -y golang make gcc packagekit mercurial
+		$sudo_command $APT install -y golang make gcc packagekit mercurial  || errorhandler
 		# one of these two golang tools packages should work on debian
-		$sudo_command $APT install -y golang-golang-x-tools || true
-		$sudo_command $APT install -y golang-go.tools || true
+		$sudo_command $APT install -y golang-golang-x-tools || errorhandler
+		$sudo_command $APT install -y golang-go.tools || errorhandler
 	fi
 	if [ ! -z "$PACMAN" ]; then
-		$sudo_command $PACMAN -S --noconfirm go
+		$sudo_command $PACMAN -S --noconfirm go  || errorhandler
 	fi
 fi
 
 # if golang is too old, we don't want to fail with an obscure error later
-if go version | grep 'go1\.[012345]\.'; then
-	echo "mgmt requires go1.6 or higher."
+if go version | grep 'go1\.[01234567]\.'; then
+	echo "mgmt requires go1.8 or higher."
 	exit 1
 fi
 
@@ -80,5 +85,11 @@ if [[ $ret != 0 ]]; then
 fi
 go get golang.org/x/tools/cmd/stringer	# for automatic stringer-ing
 go get github.com/golang/lint/golint	# for `golint`-ing
-go get -u gopkg.in/alecthomas/gometalinter.v1 && mv "$(dirname $(which gometalinter.v1))/gometalinter.v1" "$(dirname $(which gometalinter.v1))/gometalinter" && gometalinter --install	# bonus
+
+#check if its in the path if its set first
+if [ -z $GOPATH ]; then
+	echo "GOPATH isnt set, you probably should fix that!"	
+	go get -u gopkg.in/alecthomas/gometalinter.v1 && mv ~/go/bin/gometalinter.v1 ~/go/bin/gometalinter && ~/go/bin/gometalinter --install	
+	else go get -u gopkg.in/alecthomas/gometalinter.v1 && mv "$GOPATH/bin/gometalinter.v1" "$GOPATH/bin/gometalinter" && $GOPATH/bin/gometalinter --install
+fi
 cd "$XPWD" >/dev/null

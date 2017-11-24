846c5f8762f6393dfaec6a70d9b77409683c14c6
James Shubin <james@shubin.ca>
2017-11-24 09:46:32 -0500

    test: Add another check for off-by-one-error commit tags

--- test/test-commit-message.sh
+++ test/test-commit-message.sh
@@ -76,6 +76,11 @@ test_commit_message_common_bugs() {
 		echo 'FAIL: Commit message starts with `doc:`, did you mean `docs:` ?'
 		exit 1
 	fi
+	if git log --format=%s $1 | head -n 1 | grep -q "^example:"
+	then
+		echo 'FAIL: Commit message starts with `example:`, did you mean `examples:` ?'
+		exit 1
+	fi
 }
 
 if [[ -n "$TRAVIS_PULL_REQUEST_SHA" ]]

9992c367bf007a8a3d74baa91cd5014cb031ba4d
James Shubin <james@shubin.ca>
2018-02-22 02:01:14 -0500

    misc: Update golint to new location
    Somehow this got changed...

--- misc/make-deps.sh
+++ misc/make-deps.sh
@@ -92,8 +92,8 @@ fi
 go get github.com/blynn/nex				# for lexing
 go get golang.org/x/tools/cmd/goyacc			# formerly `go tool yacc`
 go get golang.org/x/tools/cmd/stringer			# for automatic stringer-ing
+go get golang.org/x/lint/golint				# for `golint`-ing
 go get github.com/tmthrgd/go-bindata/go-bindata	# for compiling in non golang files
-go get github.com/golang/lint/golint			# for `golint`-ing
 go get -u gopkg.in/alecthomas/gometalinter.v1 && mv "$(dirname $(command -v gometalinter.v1))/gometalinter.v1" "$(dirname $(command -v gometalinter.v1))/gometalinter" && gometalinter --install	# bonus
 command -v mdl &>/dev/null || gem install mdl || true	# for linting markdown files
 cd "$XPWD" >/dev/null

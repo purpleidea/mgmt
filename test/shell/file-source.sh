3715cc7b052c450f0ad20812051ba9973e3e6d01
Jonathan Gold <jonathan.e.gold@gmail.com>
2018-12-06 06:19:27 -0500

    engine: resources: file: Add shell test for source field

---
+++ test/shell/file-source.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+# vim: noet:ts=8:sts=8:sw=8
+
+set -x
+
+if ! timeout 1s sudo -A true; then
+	echo "sudo disabled: not checking file owner and group"
+	exit
+fi
+
+# run till completion
+$timeout --kill-after=30s 25s sudo -A "$MGMT" run --lang file-source.mcl --converged-timeout=5 --no-watch --tmp-prefix &
+pid=$!
+wait $pid	# get exit status
+e=$?
+
+ls -l /tmp/mgmt
+
+test -e /tmp/mgmt/file-source.txt
+cmp --silent file-source.txt /tmp/mgmt/file-source.txt || exit 1
+
+exit $e

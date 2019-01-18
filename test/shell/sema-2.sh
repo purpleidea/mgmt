88498695acce115279ce045d2035449f251071cf
James Shubin <james@shubin.ca>
2019-01-17 19:20:35 -0500

    test: Add a semaphore shell test
    This test tests new language features and as a fan in-out graph.

---
+++ test/shell/sema-2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+# should take at least 55s, but fail if we block this
+# TODO: it would be nice to make sure this test doesn't exit too early!
+$timeout --kill-after=120s 110s "$MGMT" run --converged-timeout=5 --no-watch --no-pgp --tmp-prefix lang --lang sema-2.mcl &
+pid=$!
+wait $pid	# get exit status
+exit $?

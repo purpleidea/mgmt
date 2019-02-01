f5f09d364093b7d2ebb8f8592f6af1b3e9e4e135
James Shubin <james@shubin.ca>
2019-02-01 06:58:39 -0500

    lang: funcs: Add str2int example function
    We might want to move this into a real module eventually.

---
+++ lang/funcs/core/coreexample/str2int_func.go
@@ -0,0 +1,40 @@
+// Mgmt
+// Copyright (C) 2013-2018+ James Shubin and the project contributors
+// Written by James Shubin <james@shubin.ca> and the project contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package coreexample
+
+import (
+	"strconv"
+
+	"github.com/purpleidea/mgmt/lang/funcs/simple"
+	"github.com/purpleidea/mgmt/lang/types"
+)
+
+func init() {
+	simple.ModuleRegister(moduleName, "str2int", &types.FuncValue{
+		T: types.NewType("func(a str) int"),
+		V: func(input []types.Value) (types.Value, error) {
+			var i int64
+			if val, err := strconv.ParseInt(input[0].Str(), 10, 64); err == nil {
+				i = val
+			}
+			return &types.IntValue{
+				V: i,
+			}, nil
+		},
+	})
+}

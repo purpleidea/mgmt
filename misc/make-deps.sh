10193a2796f009554b4e2e50b9a57e2f11bff93e
Lander Van den Bulcke <landervdb@inuits.eu>
2019-02-06 11:50:09 +0100

    make: Use gem --no-document instead of deprecated flags
    Signed-off-by: Lander Van den Bulcke <landervdb@inuits.eu>

--- misc/make-deps.sh
+++ misc/make-deps.sh
@@ -106,6 +106,6 @@ go get github.com/tmthrgd/go-bindata/go-bindata	# for compiling in non golang fi
 if env | grep -q -e '^TRAVIS=true$' -e '^JENKINS_URL=' -e '^BUILD_TAG=jenkins'; then
 	go get -u gopkg.in/alecthomas/gometalinter.v1 && mv "$(dirname $(command -v gometalinter.v1))/gometalinter.v1" "$(dirname $(command -v gometalinter.v1))/gometalinter" && gometalinter --install	# bonus
 fi
-command -v mdl &>/dev/null || gem install mdl --no-ri --no-rdoc || true	# for linting markdown files
-command -v fpm &>/dev/null || gem install fpm --no-ri --no-rdoc || true	# for cross distro packaging
+command -v mdl &>/dev/null || gem install mdl --no-document || true	# for linting markdown files
+command -v fpm &>/dev/null || gem install fpm --no-document || true	# for cross distro packaging
 cd "$XPWD" >/dev/null

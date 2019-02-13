f909f1453eeb1b292961550f6d49b59852f9c57d
Toshaan Bharvani <toshaan@vantosh.com>
2019-02-13 16:54:35 +0100

    lang: funcs: Added an uptime function
    Signed-off-by: Toshaan Bharvani <toshaan@vantosh.com>

---
+++ lang/funcs/core/sys/uptime_fact_posix.go
@@ -0,0 +1,32 @@
+// Mgmt
+// Copyright (C) 2013-2018+ James Shubin and the project contributors
+// Written by James Shubin <james@shubin.ca> and the project contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// +build !darwin
+
+package coresys
+
+import (
+	"syscall"
+)
+
+func uptime() (int64, error) {
+	var sysinfo syscall.Sysinfo_t
+	if err := syscall.Sysinfo(&sysinfo); err != nil {
+		return 0, err
+	}
+	return sysinfo.Uptime, nil
+}

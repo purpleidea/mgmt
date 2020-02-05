16b707dc5037751af7c4d7092b6cc787bb404817
Julien Pivotto <roidelapluie@inuits.eu>
2020-02-05 10:42:02 +0100

    instant query
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- lang/funcs/core/prometheus/instant_query.go
+++ lang/funcs/core/prometheus/instant_query.go
@@ -18,19 +18,55 @@
 package coreprometheus
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"github.com/purpleidea/mgmt/lang/funcs/simple"
 	"github.com/purpleidea/mgmt/lang/types"
+	"github.com/purpleidea/mgmt/util/errwrap"
+
+	"github.com/prometheus/client_golang/api"
+	"github.com/prometheus/client_golang/api/prometheus/v1"
+	"github.com/prometheus/common/model"
 )
 
 func init() {
-	simple.ModuleRegister(ModuleName, "instant_query", &types.FuncValue{
-		T: types.NewType("func(query str, date int, config struct{address str}) []struct{labels map{str: str}; value str}"),
-		V: InstantQuery,
+	simple.ModuleRegister(ModuleName, "instant_query_value", &types.FuncValue{
+		T: types.NewType("func(query str, ts int, config struct{address str}) str"),
+		V: InstantQueryValue,
 	})
 }
 
 // InstantQuery runs a Prometheus query at a specific time and returns the
 // output as a list of structs
-func InstantQuery(input []types.Value) (types.Value, error) {
-	return nil, nil
+func InstantQueryValue(input []types.Value) (types.Value, error) {
+	query := input[0].Str()
+	ts := input[1].Int()
+	config := input[2].Struct()
+
+	client, err := api.NewClient(api.Config{
+		Address: config["address"].Str(),
+	})
+	if err != nil {
+		return nil, errwrap.Wrapf(err, "error creating prometheus client")
+	}
+
+	api := v1.NewAPI(client)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	result, _, err := api.Query(ctx, query, time.Unix(ts, 0))
+	if err != nil {
+		return nil, errwrap.Wrapf(err, "error querying prometheus")
+	}
+	v, ok := result.(model.Vector)
+	if !ok {
+		return nil, errwrap.Wrapf(err, "error casting prometheus query result")
+	}
+	if len(v) != 1 {
+		return nil, fmt.Errorf("bad number of results: expected: 1, got: %d", len(v))
+	}
+	return &types.StrValue{
+		V: v[0].Value.String(),
+	}, nil
 }

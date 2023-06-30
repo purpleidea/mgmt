70a093623c56e803adaea61d7714873a5ad36c96
James Shubin <james@shubin.ca>
2023-06-29 23:42:58 -0400

    XXX: found some races. We can mutex this tomorrow

--- lang/ast/structs.go
+++ lang/ast/structs.go
@@ -8451,7 +8451,7 @@ func (obj *ExprCall) Graph(env map[string]interfaces.Func) (*pgraph.Graph, inter
 
 		funcValueFunc = simple.FuncValueToConstFunc(&fancyfunc.FuncValue{
 			V: func(txn interfaces.Txn, args []interfaces.Func) (interfaces.Func, error) {
-				cp, err := obj.expr.Copy()
+				cp, err := obj.expr.Copy() // XXX: I'm pretty sure this concurrent copy is causing issues. Mutex somehow.
 				if err != nil {
 					return nil, errwrap.Wrapf(err, "could not copy expression")
 				}

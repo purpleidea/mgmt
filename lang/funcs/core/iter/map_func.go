a1456ce4907a3c993a92650a44de095fb7b20fbf
James Shubin <james@shubin.ca>
2023-07-27 15:44:33 -0400

    XXX: seeing if this fixes the txn issue

--- lang/funcs/core/iter/map_func.go
+++ lang/funcs/core/iter/map_func.go
@@ -761,6 +761,8 @@ func (obj *MapFunc) replaceSubGraph(subgraphInput interfaces.Func) error {
 		Args: []string{channelBasedSinkFuncArgNameEdgeName},
 	})
 
+	edgeFns := []func(){}
+
 	for i := 0; i < obj.lastInputListLength; i++ {
 		i := i
 		inputElemFunc := simple.SimpleFnToDirectFunc(
@@ -789,12 +791,21 @@ func (obj *MapFunc) replaceSubGraph(subgraphInput interfaces.Func) error {
 			return errwrap.Wrapf(err, "could not call obj.lastFuncValue.Call()")
 		}
 
-		obj.init.Txn.AddEdge(subgraphInput, inputElemFunc, &interfaces.FuncEdge{
-			Args: []string{"inputList"},
-		})
-		obj.init.Txn.AddEdge(outputElemFunc, outputListFunc, &interfaces.FuncEdge{
-			Args: []string{fmt.Sprintf("outputElem%d", i)},
-		})
+		fn := func() {
+			obj.init.Txn.AddEdge(subgraphInput, inputElemFunc, &interfaces.FuncEdge{
+				Args: []string{"inputList"},
+			})
+			obj.init.Txn.AddEdge(outputElemFunc, outputListFunc, &interfaces.FuncEdge{
+				Args: []string{fmt.Sprintf("outputElem%d", i)},
+			})
+		}
+
+		edgeFns = append(edgeFns, fn)
+	}
+
+	// Add edges last after all the AddVertex stuff.
+	for _, fn := range edgeFns {
+		fn()
 	}
 
 	obj.init.Txn.Commit()

26036d4f8fa6c194586abeb5c76d13aa96dd307b
James Shubin <james@shubin.ca>
2023-07-30 14:48:15 -0400

    XXX: make the race actually readable

--- lang/funcs/simplepoly/simplepoly.go
+++ lang/funcs/simplepoly/simplepoly.go
@@ -495,7 +495,12 @@ func (obj *WrappedFunc) Build(typ *types.Type) (*types.Type, error) {
 // buildFunction builds our concrete static function, from the potentially
 // abstract, possibly variant containing list of functions.
 func (obj *WrappedFunc) buildFunction(typ *types.Type, ix int) *types.Type {
-	obj.fn = obj.Fns[ix].Copy().(*types.SimpleFn)
+	cp := obj.Fns[ix].Copy()
+	fn, ok := cp.(*types.SimpleFn)
+	if !ok {
+		panic("unexpected type")
+	}
+	obj.fn = fn
 	if obj.fn.T == nil { // XXX: should this even ever happen? What about argnames here?
 		obj.fn.T = typ.Copy() // overwrites any contained "variant" type
 	}

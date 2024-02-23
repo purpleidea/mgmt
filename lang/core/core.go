37569aae173cb648a417b97b27730dfe91b49ce8
James Shubin <james@shubin.ca>
2024-02-22 23:15:02 -0500

    lang: core: Add package documentation string

--- lang/core/core.go
+++ lang/core/core.go
@@ -15,6 +15,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+// Package core contains core functions and other related facilities which are
+// used in programs.
 package core
 
 import (

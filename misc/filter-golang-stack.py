90f6d4e5630ab69d737fdf7f17c81843a9c6a081
James Shubin <james@shubin.ca>
2024-03-05 01:05:50 -0500

    legal: Update http to https

--- misc/filter-golang-stack.py
+++ misc/filter-golang-stack.py
@@ -14,7 +14,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # This program filters golang stack traces. Modify the filter by editing the
 # filter_chunk function. This is a quick hack to help me get work done. It's
--- test/comment_parser.go
+++ test/comment_parser.go
@@ -13,7 +13,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 // XXX: consider using the https://pkg.go.dev/go/doc parser instead and also
 // checking the other fields like the package doc string.

3e31ee9455843771a22f76f6b75ba1d080869f80
James Shubin <james@shubin.ca>
2024-03-05 01:04:09 -0500

    legal: Additional permission under GNU GPL version 3 section 7
    With the recent merging of embedded package imports and the entry CLI
    package, it is now possible for users to build in mcl code into a single
    binary. This additional permission makes it explicitly clear that this
    is permitted to make it easier for those users. The condition is phrased
    so that the terms can be "patched" by the original author if it's
    necessary for the project. For example, if the name of the language
    (mcl) changes, has a differently named new version, someone finds a
    phrasing improvement or a legal loophole, or for some other
    reasonable circumstance. Now go write some beautiful embedded tools!

--- engine/resources/virt.go
+++ engine/resources/virt.go
@@ -14,6 +14,18 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Additional permission under GNU GPL version 3 section 7
+//
+// If you modify this program, or any covered work, by linking or combining it
+// with embedded mcl code and modules (and that the embedded mcl code and
+// modules which link with this program, contain a copy of their source code in
+// the authoritative form) containing parts covered by the terms of any other
+// license, the licensors of this program grant you additional permission to
+// convey the resulting work. Furthermore, the licensors of this program grant
+// the original author, James Shubin, additional permission to update this
+// additional permission if he deems it necessary to achieve the goals of this
+// additional permission.
 
 //go:build !novirt
 

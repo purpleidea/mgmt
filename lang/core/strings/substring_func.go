898b58e3e7d7927ad367af7a813f84431e5ba67e
James Shubin <james@shubin.ca>
2024-10-03 13:01:08 -0400

    lang: core: strings: Add a substring function named substr

---
+++ lang/core/strings/substring_func.go
@@ -0,0 +1,61 @@
+// Mgmt
+// Copyright (C) 2013-2024+ James Shubin and the project contributors
+// Written by James Shubin <james@shubin.ca> and the project contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Additional permission under GNU GPL version 3 section 7
+//
+// If you modify this program, or any covered work, by linking or combining it
+// with embedded mcl code and modules (and that the embedded mcl code and
+// modules which link with this program, contain a copy of their source code in
+// the authoritative form) containing parts covered by the terms of any other
+// license, the licensors of this program grant you additional permission to
+// convey the resulting work. Furthermore, the licensors of this program grant
+// the original author, James Shubin, additional permission to update this
+// additional permission if he deems it necessary to achieve the goals of this
+// additional permission.
+
+package corestrings
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/purpleidea/mgmt/lang/funcs/simple"
+	"github.com/purpleidea/mgmt/lang/types"
+)
+
+func init() {
+	simple.ModuleRegister(ModuleName, "substr", &simple.Scaffold{
+		T: types.NewType("func(s str, low int, high int) str"),
+		F: SubStr,
+	})
+}
+
+// SubStr returns a substring of the input string from low to high.
+func SubStr(ctx context.Context, input []types.Value) (types.Value, error) {
+	s := input[0].Str()
+	low := int(input[1].Int())
+	high := int(input[2].Int())
+	if low < 0 || low > len(s) {
+		return nil, fmt.Errorf("low/high out of range")
+	}
+	if high < 0 || high > len(s) || low > high {
+		return nil, fmt.Errorf("low/high out of range")
+	}
+	return &types.StrValue{
+		V: s[low:high],
+	}, nil
+}

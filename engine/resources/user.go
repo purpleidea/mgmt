9489740aa5f263340d9c0def4f966f0d1f11d4bc
Justin Findlay <jfindlay@cloudflare.com>
2025-02-05 09:35:30 -0500

    resources: Add shell to user resource

--- engine/resources/user.go
+++ engine/resources/user.go
@@ -78,6 +78,9 @@ type UserRes struct {
 	// HomeDir is the path to the user's home directory.
 	HomeDir *string `lang:"homedir" yaml:"homedir"`
 
+	// Shell is is the user's login shell.
+	Shell *string `lang:"shell" yaml:"shell"`
+
 	// AllowDuplicateUID is needed for a UID to be non-unique. This is rare
 	// but happens if you want more than one username to access the
 	// resources of the same UID. See the --non-unique flag in `useradd`.
@@ -224,6 +227,9 @@ func (obj *UserRes) CheckApply(ctx context.Context, apply bool) (bool, error) {
 		if obj.HomeDir != nil && *obj.HomeDir != usr.HomeDir {
 			usercheck = false
 		}
+		if obj.Shell != nil && *obj.Shell != usr.Shell {
+			usercheck = false
+		}
 		if usercheck {
 			return true, nil
 		}
@@ -260,6 +266,9 @@ func (obj *UserRes) CheckApply(ctx context.Context, apply bool) (bool, error) {
 		}
 		if obj.HomeDir != nil {
 			args = append(args, "-d", *obj.HomeDir)
+		}
+		if obj.Shell != nil {
+			args = append(args, "-s", *obj.Shell)
 		}
 	}
 	if obj.State == "absent" {
@@ -343,11 +352,19 @@ func (obj *UserRes) Cmp(r engine.Res) error {
 		}
 	}
 	if (obj.HomeDir == nil) != (res.HomeDir == nil) {
-		return fmt.Errorf("the HomeDirs differs")
+		return fmt.Errorf("the HomeDir differs")
 	}
 	if obj.HomeDir != nil && res.HomeDir != nil {
 		if *obj.HomeDir != *res.HomeDir {
 			return fmt.Errorf("the HomeDir differs")
+		}
+	}
+	if (obj.Shell == nil) != (res.Shell == nil) {
+		return fmt.Errorf("the Shell differs")
+	}
+	if obj.Shell != nil && res.Shell != nil {
+		if *obj.Shell != *res.Shell {
+			return fmt.Errorf("the Shell differs")
 		}
 	}
 	if obj.AllowDuplicateUID != res.AllowDuplicateUID {

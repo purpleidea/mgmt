8703a667d916985a306e7d6a94b1c669c8a4fa19
James Shubin <james@shubin.ca>
2025-11-30 19:29:08 -0500

    misc: Add wasm file for baddev

--- misc/make-baddev.sh
+++ misc/make-baddev.sh
@@ -6,6 +6,7 @@ git add lang/parser/lexer.nn.go --force
 git add lang/parser/y.go --force
 git add lang/interpolate/parse.generated.go --force
 git add lang/core/generated_funcs.go --force
+git add engine/resources/http_server_ui/main.wasm --force
 git commit -m 'lang: Commit generated code'
 git push origin baddev --force
 echo 'run `make baddev` to build a binary!'

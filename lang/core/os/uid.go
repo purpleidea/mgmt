afee651c292f86cf0c3e334b5ebf61c2bf553b17
James Shubin <james@shubin.ca>
2025-12-02 21:17:19 -0500

    lang: core: os: Add a uid function

---
+++ lang/core/os/uid.go
@@ -0,0 +1,64 @@
+// Mgmt
+// Copyright (C) James Shubin and the project contributors
+// Written by James Shubin <james@shubin.ca> and the project contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Additional permission under GNU GPL version 3 section 7
+//
+// If you modify this program, or any covered work, by linking or combining it
+// with embedded mcl code and modules (and that the embedded mcl code and
+// modules which link with this program, contain a copy of their source code in
+// the authoritative form) containing parts covered by the terms of any other
+// license, the licensors of this program grant you additional permission to
+// convey the resulting work. Furthermore, the licensors of this program grant
+// the original author, James Shubin, additional permission to update this
+// additional permission if he deems it necessary to achieve the goals of this
+// additional permission.
+
+package coreos
+
+import (
+	"context"
+	"os/user"
+
+	"github.com/purpleidea/mgmt/lang/funcs/simple"
+	"github.com/purpleidea/mgmt/lang/types"
+)
+
+func init() {
+	simple.ModuleRegister(ModuleName, "uid", &simple.Scaffold{
+		// These might change depending on user database...
+		I: &simple.Info{
+			Pure: false,
+			Memo: false,
+			Fast: true,
+			Spec: false,
+		},
+		T: types.NewType("func(str) str"),
+		F: UID,
+	})
+}
+
+// UID returns the uid of the user. This is a string actually.
+func UID(ctx context.Context, input []types.Value) (types.Value, error) {
+	usr, err := user.Lookup(input[0].Str())
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.StrValue{
+		V: usr.Uid,
+	}, nil
+}

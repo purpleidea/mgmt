purpleidea/mgmt

commit b44098ee4f4ce281bc5319d5565a6612ef5bac69

lang: Improve map
Our map type had two main issues. Firstly, it compared pointers instead
of comparing values when doing Cmp. Amazingly, this didn't turn out to
be a serious problem for many obscure reasons. Granted, it definitely
needed fixing. Secondly, we weren't fully implementing a proper map, we
were just using the golang map and weren't doing any hashing. This has
all now been fixed.

Spectacularly, it didn't have much of a negative effect at all, it was
really only a new test that officially caught this issue, even though I
had this feature branch pending since before golang 1.24 landed.

Lastly, my code for handling the struct hashing and everything else may
in fact not be optimal. I'm not the perfect algorithmist, so if I got it
wrong, I'm happy for the pros to correct me or tidy this up!

lang/ast/structs.go
@@ -8420,7 +8420,6 @@ func (obj *ExprMap) Init(data *interfaces.Data) error {
 	obj.data = data
 	obj.Textarea.Setup(data)
 
-	// XXX: Can we check that there aren't any duplicate keys? Can we Cmp?
 	for _, x := range obj.KVs {
 		if err := x.Key.Init(data); err != nil {
 			return err
@@ -8429,6 +8428,59 @@ func (obj *ExprMap) Init(data *interfaces.Data) error {
 			return err
 		}
 	}
+
+	// Here we statically check that there aren't any duplicate keys. To do
+	// so we also end up checking if there are any inconsistent types. Keep
+	// in mind that we can't guarantee that we won't fail later during type
+	// unification (a key could be coming from a variable like $foo) but we
+	// can catch the static cases that contain duplicates.
+	keys := []types.Value{}
+	var typ *types.Type
+	for _, x := range obj.KVs {
+		if t, err := x.Key.Type(); err == nil {
+			if typ == nil {
+				typ = t // save it
+			} else if err := typ.Cmp(t); err != nil {
+				// TODO: wrap error here to make it user friendly?
+				return errwrap.Wrapf(err, "inconsistent map key type")
+			}
+		}
+
+		val, err := x.Key.Value()
+		if err != nil { // can't determine the value statically
+			continue
+		}
+		keys = append(keys, val)
+	}
+	if typ == nil {
+		return nil // can't do more without knowing the type
+	}
+
+	// This check is useful to catch these errors before type unification!
+	if !types.IsHashableType(typ) {
+		return fmt.Errorf("map key type %s is not hashable", typ)
+	}
+
+	// The `nil` type is not important, we just need any stand-in.
+	mapTyp := types.NewType(fmt.Sprintf("map{%s: nil}", typ.String()))
+	m := mapTyp.New().(*types.MapValue)
+	if m == nil {
+		// If you build a map with an invalid type, then it will be nil.
+		return fmt.Errorf("map key type %s is not hashable", typ.String())
+	}
+
+	// Instead of N^2 Cmp check here, try and add them to the map.
+	for i, key := range keys {
+		if err := m.Set(key, types.ValueNil); err != nil {
+			// TODO: wrap error here to make it user friendly?
+			return err // invalid key
+		}
+		if m.Len() != i+1 { // Length mismatch!
+			// We must have just added a duplicate!
+			return fmt.Errorf("duplicate map key: %s", key)
+		}
+	}
+
 	return nil
 }
 
@@ -8598,6 +8650,14 @@ func (obj *ExprMap) SetScope(scope *interfaces.Scope, sctx map[string]interfaces
 // previously been set (when not initially known) this will error.
 func (obj *ExprMap) SetType(typ *types.Type) error {
 	// TODO: should we ensure this is set to a KindMap ?
+	if typ == nil || typ.Key == nil {
+		return fmt.Errorf("type is nil")
+	}
+	// This check is necessary to catch this after type unification!
+	if !types.IsHashableType(typ.Key) {
+		return fmt.Errorf("map key type %s is not hashable", typ.Key)
+	}
+
 	if obj.typ != nil {
 		return obj.typ.Cmp(typ) // if not set, ensure it doesn't change
 	}
@@ -8804,8 +8864,10 @@ func (obj *ExprMap) SetValue(value types.Value) error {
 // This might get called speculatively (early) during unification to learn more.
 func (obj *ExprMap) Value() (types.Value, error) {
 	kvs := make(map[types.Value]types.Value)
+	keys := []types.Value{}
 	var ktyp, vtyp *types.Type
 
+	// A subsequent key overwrites an earlier one.
 	for i, x := range obj.KVs {
 		// keys
 		kt, err := x.Key.Type()
@@ -8847,7 +8909,9 @@ func (obj *ExprMap) Value() (types.Value, error) {
 			return nil, fmt.Errorf("val for map index `%d` was nil", i)
 		}
 
-		kvs[key] = val // add to map
+		// Use proper Set API below so we don't add duplicates!
+		kvs[key] = val           // add to temporary map
+		keys = append(keys, key) // save the order
 	}
 	if len(obj.KVs) > 0 {
 		t := &types.Type{
@@ -8862,10 +8926,19 @@ func (obj *ExprMap) Value() (types.Value, error) {
 		}
 	}
 
-	return &types.MapValue{
-		T: obj.typ,
-		V: kvs,
-	}, nil
+	if obj.typ == nil {
+		return nil, fmt.Errorf("can't build value with unknown type")
+	}
+
+	// Build this safely with the correct API.
+	m := obj.typ.New().(*types.MapValue)
+
+	for _, key := range keys {
+		if err := m.Set(key, kvs[key]); err != nil {
+			return nil, err
+		}
+	}
+	return m, nil // *types.MapValue
 }
 
 // ExprMapKV represents a key and value pair in a (dictionary) map. This does

lang/types/value.go
@@ -51,6 +51,11 @@ var (
 	// zero reflect.Value.
 	ErrInvalidValue = errors.New("cannot represent invalid reflect.Value")
 
+	// ValueNil is a nil value in our system. Can be used where needed, must
+	// not be exposed to the end user in the mcl language.
+	//ValueNil, _ = ValueOfGolang(nil) // doesn't work how we expect atm
+	ValueNil = &NilValue{}
+
 	// ValueFalse is a false value in our system. Can be used where needed.
 	ValueFalse, _ = ValueOfGolang(false)
 
@@ -82,6 +87,7 @@ type Value interface {
 // reminder that if you pass in a nil value, or something containing a nil
 // value, then you won't get what you want. See our documentation for ValueOf.
 func ValueOfGolang(i interface{}) (Value, error) {
+	// XXX: Should a nil return a &NilValue{} here?
 	return ValueOf(reflect.ValueOf(i))
 }
 
@@ -700,6 +706,12 @@ func (obj *BoolValue) Value() interface{} {
 	return obj.V
 }
 
+// Hash hashes this value to provide a unique key for referencing this in a map.
+func (obj *BoolValue) Hash(seed Seed) Hash {
+	//return Comparable[bool](seed, obj.V)
+	return Comparable(seed, obj.V)
+}
+
 // Bool represents the value of this type as a bool if it is one. If this is not
 // a bool, then this panics.
 func (obj *BoolValue) Bool() bool {
@@ -755,6 +767,12 @@ func (obj *StrValue) Value() interface{} {
 	return obj.V
 }
 
+// Hash hashes this value to provide a unique key for referencing this in a map.
+func (obj *StrValue) Hash(seed Seed) Hash {
+	//return Comparable[string](seed, obj.V)
+	return Comparable(seed, obj.V)
+}
+
 // Str represents the value of this type as a string if it is one. If this is
 // not a string, then this panics.
 func (obj *StrValue) Str() string {
@@ -807,6 +825,12 @@ func (obj *IntValue) Copy() Value {
 // Value returns the raw value of this type.
 func (obj *IntValue) Value() interface{} {
 	return obj.V
+}
+
+// Hash hashes this value to provide a unique key for referencing this in a map.
+func (obj *IntValue) Hash(seed Seed) Hash {
+	//return Comparable[int64](seed, obj.V)
+	return Comparable(seed, obj.V)
 }
 
 // Int represents the value of this type as an integer if it is one. If this is
@@ -864,6 +888,12 @@ func (obj *FloatValue) Copy() Value {
 // Value returns the raw value of this type.
 func (obj *FloatValue) Value() interface{} {
 	return obj.V
+}
+
+// Hash hashes this value to provide a unique key for referencing this in a map.
+func (obj *FloatValue) Hash(seed Seed) Hash {
+	//return Comparable[float64](seed, obj.V)
+	return Comparable(seed, obj.V)
 }
 
 // Float represents the value of this type as a float if it is one. If this is
@@ -1006,22 +1036,48 @@ func (obj *ListValue) Contains(v Value) (index int, exists bool) {
 	return -1, false
 }
 
-// MapValue represents a dictionary value.
+// MapValue represents a dictionary value. The keys must all have the same type.
+// The values must all have the same type. The type of the keys must be one that
+// is hashable. At the moment this means that only bool, str, int, float are
+// supported. Structs with valid hashable keys are also allowed.
 type MapValue struct {
 	Base
 	// the types of all keys and values are represented inside of T
 	V map[Value]Value
 	T *Type
+
+	// m is a map from hash to a list of values being stored that hash to
+	// this. It's a list in case there are hash collisions. The stored value
+	// is the pointer to the key in the above map storing the actual value.
+	m map[Hash][]Value
+
+	// s is the stored seed for this map.
+	s Seed
+
+	// TODO: If we needed to order the keys deterministically, we could add
+	// each hash here. For multiple keys that hash to the same value, we
+	// order them based on the above list order.
+	//l []Hash // do we need to order the keys?
 }
 
-// NewMap creates a new map with the specified map type.
+// NewMap creates a new map with the specified map type. It's important to use
+// this when creating a map because it initializes the internal private
+// structures. If you attempt to make this with a key type that is not hashable,
+// then this will panic!
 func NewMap(t *Type) *MapValue {
 	if t.Kind != KindMap {
 		return nil // sanity check
 	}
+	if !IsHashableType(t.Key) {
+		// key is not hashable
+		return nil
+	}
+
 	return &MapValue{
 		V: make(map[Value]Value),
 		T: t,
+		m: make(map[Hash][]Value),
+		s: MakeSeed(),
 	}
 }
 
@@ -1065,14 +1121,15 @@ func (obj *MapValue) Cmp(val Value) error {
 		return fmt.Errorf("maps have different lengths")
 	}
 
-	for k := range obj.V {
-		v, exists := cmp.V[k]
+	for key := range obj.V {
+		//val, exists := cmp.V[key] // wrong!
+		val, exists := cmp.Lookup(key)
 		if !exists {
-			return fmt.Errorf("key %s does not exist", k)
+			return fmt.Errorf("key %s does not exist", key)
 		}
 
-		if err := obj.V[k].Cmp(v); err != nil {
-			return errwrap.Wrapf(err, "index %s did not cmp", k)
+		if err := obj.V[key].Cmp(val); err != nil {
+			return errwrap.Wrapf(err, "key %s did not cmp", key)
 		}
 	}
 
@@ -1082,12 +1139,24 @@ func (obj *MapValue) Cmp(val Value) error {
 // Copy returns a copy of this value.
 func (obj *MapValue) Copy() Value {
 	m := map[Value]Value{}
-	for k, v := range obj.V {
-		m[k.Copy()] = v.Copy()
+	newM := make(map[Hash][]Value)
+	for key, val := range obj.V {
+		newKey := key.Copy()
+		newVal := val.Copy()
+
+		hash := newKey.(Hashable).Hash(obj.s) // must not panic
+		if _, exists := newM[hash]; !exists {
+			newM[hash] = make([]Value, 1) // needs at least one entry
+		}
+		newM[hash] = append(newM[hash], newKey)
+		m[newKey] = newVal
 	}
+
 	return &MapValue{
 		V: m,
 		T: obj.T.Copy(),
+		m: newM,
+		s: MakeSeed(),
 	}
 }
 
@@ -1103,7 +1172,9 @@ func (obj *MapValue) Value() interface{} {
 }
 
 // Map represents the value of this type as a dictionary if it is one. If this
-// is not a map, then this panics.
+// is not a map, then this panics. Remember that the keys of this map are the
+// pointers to the Value objects. You *cannot* use the standard _, exists := map
+// syntax to check if a key exists. Instead use the Lookup function!
 func (obj *MapValue) Map() map[Value]Value {
 	return obj.V
 }
@@ -1113,34 +1184,136 @@ func (obj *MapValue) Len() int {
 	return len(obj.V)
 }
 
-// Add adds an element to this map. It errors if the types do not match.
-func (obj *MapValue) Add(k, v Value) error { // TODO: change method name?
-
+// Set adds an element to this map. It errors if the types do not match. If the
+// key already exists, this will overwrite it.
+func (obj *MapValue) Set(key, val Value) error { // TODO: change method name?
 	//if obj.T.Key.Kind != KindVariant {
-	if err := obj.T.Key.Cmp(k.Type()); err != nil {
+	if err := obj.T.Key.Cmp(key.Type()); err != nil {
 		return errwrap.Wrapf(err, "key does not match map key type")
 	}
 	//}
-
 	if obj.T.Val.Kind != KindVariant { // skip cmp if dest is a variant
-		if err := obj.T.Val.Cmp(v.Type()); err != nil {
+		if err := obj.T.Val.Cmp(val.Type()); err != nil {
 			return errwrap.Wrapf(err, "val does not match map val type")
 		}
 	}
 
-	obj.V[k] = v
+	if typ := key.Type(); !IsHashableType(typ) {
+		return fmt.Errorf("map key type %s is not hashable", typ)
+	}
+	hashable, ok := key.(Hashable)
+	if !ok {
+		return fmt.Errorf("map key type %s is not hashable", key.Type())
+	}
+
+	hash := hashable.Hash(obj.s)
+	if _, exists := obj.m[hash]; !exists {
+		obj.m[hash] = make([]Value, 1) // needs at least one entry
+	}
+	index := -1
+	var found Value
+	for i, x := range obj.m[hash] { // []Value
+		// the nil check is in case we made a value above
+		if x == nil || x.Cmp(key) != nil {
+			continue
+		}
+		index = i
+		found = x
+		break // assume datastructure doesn't have invalid dupes
+	}
+	if index == -1 {
+		index = 0 // set to first entry
+	} else {
+		delete(obj.V, found) // remove old key (we're overwriting)
+	}
+	obj.m[hash][index] = key
+	obj.V[key] = val // store!
+
 	return nil
 }
 
-// Lookup searches the map for a key. On success it also returns the Value.
+// Lookup searches the map for a key. On success it also returns the Value. If
+// the key is of the wrong type, this will return false as well.
 func (obj *MapValue) Lookup(key Value) (value Value, exists bool) {
 	//v, exists := obj.V[key] // not what we want!
-	for k, v := range obj.V {
-		if k.Cmp(key) == nil {
-			return v, true // found
-		}
+
+	//for k, v := range obj.V { // slow version
+	//	if k.Cmp(key) == nil {
+	//		return v, true // found
+	//	}
+	//}
+
+	//if obj.T.Key.Kind != KindVariant {
+	if err := obj.T.Key.Cmp(key.Type()); err != nil {
+		return nil, false
 	}
-	return nil, false
+	//}
+
+	hashable, ok := key.(Hashable)
+	if !ok {
+		//return fmt.Errorf("type %s is not hashable", k.Type())
+		return nil, false
+	}
+
+	hash := hashable.Hash(obj.s)
+	if _, exists := obj.m[hash]; !exists {
+		return nil, false // not found
+	}
+
+	for _, x := range obj.m[hash] { // []Value
+		if x.Cmp(key) != nil {
+			continue
+		}
+
+		val, exists := obj.V[x]
+		if !exists {
+			// programming error or someone poked at the struct
+			panic("inconsistent map structure")
+		}
+		return val, true // found!
+	}
+
+	return nil, false // not found
+}
+
+// Delete deletes the key from the map. It returns true if it existed, false
+// otherwise. If the key is of the wrong type, this will return false as well.
+func (obj *MapValue) Delete(key Value) (exists bool) {
+	//if obj.T.Key.Kind != KindVariant {
+	if err := obj.T.Key.Cmp(key.Type()); err != nil {
+		return false
+	}
+	//}
+
+	hashable, ok := key.(Hashable)
+	if !ok {
+		return false
+		//return fmt.Errorf("type %s is not hashable", key.Type())
+	}
+
+	hash := hashable.Hash(obj.s)
+	if _, exists := obj.m[hash]; !exists {
+		return false // not found
+	}
+	index := -1
+	var found Value
+	for i, x := range obj.m[hash] { // []Value
+		if x.Cmp(key) != nil {
+			continue
+		}
+		index = i
+		found = x
+		break // assume datastructure doesn't have invalid dupes
+	}
+	if index == -1 {
+		return false // not found
+	}
+
+	// nuke that element in our list
+	obj.m[hash] = append(obj.m[hash][:index], obj.m[hash][index+1:]...)
+	delete(obj.V, found) // remove old key (we're overwriting)
+
+	return true // deleted
 }
 
 // StructValue represents a struct value. The keys are ordered.
@@ -1227,6 +1400,27 @@ func (obj *StructValue) Value() interface{} {
 		val.FieldByName(k).Set(reflect.ValueOf(obj.V[k].Value())) // recurse
 	}
 	return val.Interface()
+}
+
+// Hash hashes this value to provide a unique key for referencing this in a map.
+func (obj *StructValue) Hash(seed Seed) Hash {
+	if obj.V == nil {
+		panic("malformed struct")
+	}
+	if obj.T == nil {
+		panic("malformed struct type")
+	}
+	if len(obj.V) != len(obj.T.Ord) {
+		panic("malformed struct length")
+	}
+	if len(obj.T.Map) != len(obj.T.Ord) {
+		panic("malformed struct length")
+	}
+
+	h := NewHasher(seed) // *Hasher
+	return h.Hash(obj)
+	//return Comparable[?](seed, obj.V)
+	//return Comparable(seed, obj.V)
 }
 
 // Struct represents the value of this type as a struct if it is one. If this is

examples/lang/map-duplicate.mcl
@@ -1,0 +1,22 @@
+import "datetime"
+import "fmt"
+import "math"
+
+$now = datetime.now()
+$x = math.mod($now, 2)
+
+$key = if $x == 0 {
+	"k1"
+} else {
+	"k2"
+}
+
+$m = {"k1" => 42, $key => 13,} # dupe!
+
+# Later value gets chosen.
+#$found = $m["k1"] || 99
+$found = $m["k2"] || 99
+
+print "print1" {
+	msg => fmt.printf("found value of: %d", $found),
+}

lang/core/sys/env.go
@@ -111,14 +111,21 @@ func HasEnv(ctx context.Context, input []types.Value) (types.Value, error) {
 
 // Env returns a map of all keys and their values.
 func Env(ctx context.Context, input []types.Value) (types.Value, error) {
-	environ := make(map[types.Value]types.Value)
+	m := types.NewType("map{str: str}").New().(*types.MapValue)
+
 	for _, keyval := range os.Environ() {
-		if i := strings.IndexRune(keyval, '='); i != -1 {
-			environ[&types.StrValue{V: keyval[:i]}] = &types.StrValue{V: keyval[i+1:]}
+		i := strings.IndexRune(keyval, '=')
+		if i == -1 {
+			continue
+		}
+
+		k := &types.StrValue{V: keyval[:i]}
+		v := &types.StrValue{V: keyval[i+1:]}
+
+		if err := m.Set(k, v); err != nil {
+			return nil, err
 		}
 	}
-	return &types.MapValue{
-		T: types.NewType("map{str: str}"),
-		V: environ,
-	}, nil
+
+	return m, nil
 }

lang/core/world/kvlookup.go
@@ -199,7 +199,7 @@ func (obj *KVLookupFunc) Call(ctx context.Context, args []types.Value) (types.Va
 	for k, v := range keyMap {
 		key := &types.StrValue{V: k}
 		val := &types.StrValue{V: v}
-		if err := d.Add(key, val); err != nil {
+		if err := d.Set(key, val); err != nil {
 			return nil, errwrap.Wrapf(err, "map could not add key `%s`, val: `%s`", k, v)
 		}
 	}

commit d6515df0ca4166ad3a07d648b7007287c5d8b546

lang: ast, funcs: structs: Fix forkv map regression
After b44098ee4f4ce281bc5319d5565a6612ef5bac69 I missed porting this one
map lookup, which caused the loop to error if it received new values.

lang/ast/structs.go
@@ -4601,14 +4601,27 @@ func (obj *StmtForKV) Output(table interfaces.Table) (*interfaces.Output, error)
 
 	m := expr.Map() // must not panic!
 
+	// We need to use this Cmp loop, because we pull the key from the map if
+	// it has the same *value* not the same *pointer* of course...
+	lookupIterBody := func(key types.Value) (interfaces.Stmt, bool) {
+		for k, v := range obj.iterBody {
+			if k.Cmp(key) == nil {
+				return v, true
+			}
+		}
+		return nil, false
+	}
+
 	for key := range m {
 		// key and val are both an mcl types.Value
 		// XXX: Do we need a mutex around this iterBody access?
-		if _, exists := obj.iterBody[key]; !exists {
+		//stmt, exists := obj.iterBody[key] // ptr not equivalent, use Cmp!
+		stmt, exists := lookupIterBody(key) // woo!
+		if !exists {
 			// programming error
 			return nil, fmt.Errorf("programming error on key: %s", key)
 		}
-		output, err := obj.iterBody[key].Output(table)
+		output, err := stmt.Output(table)
 		if err != nil {
 			return nil, err
 		}

lang/funcs/structs/forkv.go
@@ -179,7 +179,13 @@ func (obj *ForKVFunc) replaceSubGraph(subgraphInput interfaces.Func) error {
 					if !ok {
 						return nil, fmt.Errorf("inputElemFuncVal: expected a MapValue argument")
 					}
-					return m.Map()[ptr], nil
+
+					val, exists := m.Lookup(ptr)
+					if !exists {
+						// programming error
+						return nil, fmt.Errorf("unexpected missing key: %v", ptr)
+					}
+					return val, nil
 				},
 				T: types.NewType(fmt.Sprintf("func(%s %s) %s", argNameVal, obj.mapType(), obj.ValType)),
 			},

lang/types/value.go
@@ -1575,6 +1575,7 @@ func (obj *FuncValue) Call(ctx context.Context, args []Value) (Value, error) {
 	result, err := obj.V(ctx, args) // call it
 	if result == nil {
 		if err == nil {
+			// programming error?
 			return nil, fmt.Errorf("function returned nil result")
 		}
 		return nil, err

lang/core/test/forkvlooptest.go
@@ -1,0 +1,154 @@
+// Mgmt
+// Copyright (C) James Shubin and the project contributors
+// Written by James Shubin <james@shubin.ca> and the project contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Additional permission under GNU GPL version 3 section 7
+//
+// If you modify this program, or any covered work, by linking or combining it
+// with embedded mcl code and modules (and that the embedded mcl code and
+// modules which link with this program, contain a copy of their source code in
+// the authoritative form) containing parts covered by the terms of any other
+// license, the licensors of this program grant you additional permission to
+// convey the resulting work. Furthermore, the licensors of this program grant
+// the original author, James Shubin, additional permission to update this
+// additional permission if he deems it necessary to achieve the goals of this
+// additional permission.
+
+package coretest
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/purpleidea/mgmt/lang/funcs"
+	"github.com/purpleidea/mgmt/lang/interfaces"
+	"github.com/purpleidea/mgmt/lang/types"
+)
+
+const (
+	// ForKVLoopFuncName is the name this function is registered as.
+	ForKVLoopFuncName = "forkvloop"
+)
+
+func init() {
+	funcs.ModuleRegister(ModuleName, ForKVLoopFuncName, func() interfaces.Func { return &ForKVLoopFunc{} }) // must register the func and name
+}
+
+// ForKVLoopFunc is a function that is used for testing.
+type ForKVLoopFunc struct {
+	init *interfaces.Init
+
+	count int
+}
+
+// String returns a simple name for this function. This is needed so this struct
+// can satisfy the pgraph.Vertex interface.
+func (obj *ForKVLoopFunc) String() string {
+	return ForKVLoopFuncName
+}
+
+// ArgGen returns the Nth arg name for this function.
+func (obj *ForKVLoopFunc) ArgGen(index int) (string, error) {
+	seq := []string{}
+	if l := len(seq); index >= l {
+		return "", fmt.Errorf("index %d exceeds arg length of %d", index, l)
+	}
+	return seq[index], nil
+}
+
+// Validate makes sure we've built our struct properly. It is usually unused for
+// normal functions that users can use directly.
+func (obj *ForKVLoopFunc) Validate() error {
+	return nil
+}
+
+// Info returns some static info about itself.
+func (obj *ForKVLoopFunc) Info() *interfaces.Info {
+	return &interfaces.Info{
+		Pure: false, // maybe false because the output changes
+		Memo: false,
+		Fast: false,
+		Spec: false,
+		Sig:  types.NewType("func() map{str: str}"),
+	}
+}
+
+// Init runs some startup code for this function.
+func (obj *ForKVLoopFunc) Init(init *interfaces.Init) error {
+	obj.init = init
+	return nil
+}
+
+// Stream returns the changing values that this func has over time.
+func (obj *ForKVLoopFunc) Stream(ctx context.Context) error {
+	//defer close(obj.input)  // if we close, this is a race with the sender
+
+	startup := make(chan struct{})
+	close(startup)
+
+	for {
+		select {
+		case <-startup:
+			startup = nil
+			// send an initial event
+
+		case <-time.After(5 * time.Second):
+
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+
+		if err := obj.init.Event(ctx); err != nil { // send event
+			return err
+		}
+	}
+}
+
+// Call this function with the input args and return the value if it is possible
+// to do so at this time.
+func (obj *ForKVLoopFunc) Call(ctx context.Context, args []types.Value) (types.Value, error) {
+	// Check before we send to a chan where we'd need Stream to be running.
+	if obj.init == nil {
+		return nil, funcs.ErrCantSpeculate
+	}
+
+	m := types.NewMap(types.NewType("map{str: str}"))
+
+	//if obj.count == 0 {
+	//	if err := m.Set(&types.StrValue{V: "hello"}, &types.StrValue{V: "world"}); err != nil {
+	//		return nil, err
+	//	}
+	//	obj.count++
+	//	return m, nil
+	//}
+
+	if err := m.Set(&types.StrValue{V: "foo"}, &types.StrValue{V: "bar"}); err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+// Cleanup runs after that function was removed from the graph.
+func (obj *ForKVLoopFunc) Cleanup(ctx context.Context) error {
+	return nil
+}
+
+// Done is a message from the engine to tell us that no more Call's are coming.
+func (obj *ForKVLoopFunc) Done() error {
+	return nil
+}

lang/core/test/forlooptest.go
@@ -1,0 +1,154 @@
+// Mgmt
+// Copyright (C) James Shubin and the project contributors
+// Written by James Shubin <james@shubin.ca> and the project contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Additional permission under GNU GPL version 3 section 7
+//
+// If you modify this program, or any covered work, by linking or combining it
+// with embedded mcl code and modules (and that the embedded mcl code and
+// modules which link with this program, contain a copy of their source code in
+// the authoritative form) containing parts covered by the terms of any other
+// license, the licensors of this program grant you additional permission to
+// convey the resulting work. Furthermore, the licensors of this program grant
+// the original author, James Shubin, additional permission to update this
+// additional permission if he deems it necessary to achieve the goals of this
+// additional permission.
+
+package coretest
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/purpleidea/mgmt/lang/funcs"
+	"github.com/purpleidea/mgmt/lang/interfaces"
+	"github.com/purpleidea/mgmt/lang/types"
+)
+
+const (
+	// ForLoopFuncName is the name this function is registered as.
+	ForLoopFuncName = "forloop"
+)
+
+func init() {
+	funcs.ModuleRegister(ModuleName, ForLoopFuncName, func() interfaces.Func { return &ForLoopFunc{} }) // must register the func and name
+}
+
+// ForLoopFunc is a function that is used for testing.
+type ForLoopFunc struct {
+	init *interfaces.Init
+
+	count int
+}
+
+// String returns a simple name for this function. This is needed so this struct
+// can satisfy the pgraph.Vertex interface.
+func (obj *ForLoopFunc) String() string {
+	return ForLoopFuncName
+}
+
+// ArgGen returns the Nth arg name for this function.
+func (obj *ForLoopFunc) ArgGen(index int) (string, error) {
+	seq := []string{}
+	if l := len(seq); index >= l {
+		return "", fmt.Errorf("index %d exceeds arg length of %d", index, l)
+	}
+	return seq[index], nil
+}
+
+// Validate makes sure we've built our struct properly. It is usually unused for
+// normal functions that users can use directly.
+func (obj *ForLoopFunc) Validate() error {
+	return nil
+}
+
+// Info returns some static info about itself.
+func (obj *ForLoopFunc) Info() *interfaces.Info {
+	return &interfaces.Info{
+		Pure: false, // maybe false because the output changes
+		Memo: false,
+		Fast: false,
+		Spec: false,
+		Sig:  types.NewType("func() []str"),
+	}
+}
+
+// Init runs some startup code for this function.
+func (obj *ForLoopFunc) Init(init *interfaces.Init) error {
+	obj.init = init
+	return nil
+}
+
+// Stream returns the changing values that this func has over time.
+func (obj *ForLoopFunc) Stream(ctx context.Context) error {
+	//defer close(obj.input)  // if we close, this is a race with the sender
+
+	startup := make(chan struct{})
+	close(startup)
+
+	for {
+		select {
+		case <-startup:
+			startup = nil
+			// send an initial event
+
+		case <-time.After(5 * time.Second):
+
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+
+		if err := obj.init.Event(ctx); err != nil { // send event
+			return err
+		}
+	}
+}
+
+// Call this function with the input args and return the value if it is possible
+// to do so at this time.
+func (obj *ForLoopFunc) Call(ctx context.Context, args []types.Value) (types.Value, error) {
+	// Check before we send to a chan where we'd need Stream to be running.
+	if obj.init == nil {
+		return nil, funcs.ErrCantSpeculate
+	}
+
+	l := types.NewList(types.NewType("[]str"))
+
+	if obj.count == 0 {
+		if err := l.Add(&types.StrValue{V: "foo"}); err != nil {
+			return nil, err
+		}
+		obj.count++
+		return l, nil
+	}
+
+	if err := l.Add(&types.StrValue{V: "bar"}); err != nil {
+		return nil, err
+	}
+
+	return l, nil
+}
+
+// Cleanup runs after that function was removed from the graph.
+func (obj *ForLoopFunc) Cleanup(ctx context.Context) error {
+	return nil
+}
+
+// Done is a message from the engine to tell us that no more Call's are coming.
+func (obj *ForLoopFunc) Done() error {
+	return nil
+}

lang/interpret_test/TestAstFunc2/bug-forkv-nil1.txtar
@@ -1,0 +1,9 @@
+-- main.mcl --
+import "test"
+forkv $k, $v in test.forkvloop() { # causes bug when forkv map stuff is broken
+	test "${k}-${v}" {}
+}
+# without the fix we get this error:
+# gapi exited with error: function returned nil result
+-- OUTPUT --
+Vertex: test[foo-bar]
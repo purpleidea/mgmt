14af8747dba0dcea86c9716fcd03919ebf6afec6
James Shubin <james@shubin.ca>
2026-03-19 17:22:58 -0400

    lang: core: deploy: add a file exists function for deploys

---
+++ lang/core/deploy/file_exists.go
@@ -0,0 +1,179 @@
+// Mgmt
+// Copyright (C) James Shubin and the project contributors
+// Written by James Shubin <james@shubin.ca> and the project contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Additional permission under GNU GPL version 3 section 7
+//
+// If you modify this program, or any covered work, by linking or combining it
+// with embedded mcl code and modules (and that the embedded mcl code and
+// modules which link with this program, contain a copy of their source code in
+// the authoritative form) containing parts covered by the terms of any other
+// license, the licensors of this program grant you additional permission to
+// convey the resulting work. Furthermore, the licensors of this program grant
+// the original author, James Shubin, additional permission to update this
+// additional permission if he deems it necessary to achieve the goals of this
+// additional permission.
+
+package coredeploy
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/purpleidea/mgmt/lang/funcs"
+	"github.com/purpleidea/mgmt/lang/interfaces"
+	"github.com/purpleidea/mgmt/lang/types"
+	"github.com/purpleidea/mgmt/util/errwrap"
+)
+
+const (
+	// FileExistsFuncName is the name this function is registered as.
+	FileExistsFuncName = "file_exists"
+
+	// arg names...
+	fileExistsArgNameFilename = "filename"
+)
+
+func init() {
+	funcs.ModuleRegister(ModuleName, FileExistsFuncName, func() interfaces.Func { return &FileExistsFunc{} }) // must register the func and name
+}
+
+// FileExistsFunc is a function that returns if a deployed file exists or not.
+// This works with directories too. The file existence can only change with a
+// new deploy, so this is static. Please note that this is different from the
+// file_exists function in the os package.
+type FileExistsFunc struct {
+	interfaces.Textarea
+
+	init *interfaces.Init
+	data *interfaces.FuncData
+}
+
+// String returns a simple name for this function. This is needed so this struct
+// can satisfy the pgraph.Vertex interface.
+func (obj *FileExistsFunc) String() string {
+	return FileExistsFuncName
+}
+
+// SetData is used by the language to pass our function some code-level context.
+func (obj *FileExistsFunc) SetData(data *interfaces.FuncData) {
+	obj.data = data
+}
+
+// ArgGen returns the Nth arg name for this function.
+func (obj *FileExistsFunc) ArgGen(index int) (string, error) {
+	seq := []string{fileExistsArgNameFilename}
+	if l := len(seq); index >= l {
+		return "", fmt.Errorf("index %d exceeds arg length of %d", index, l)
+	}
+	return seq[index], nil
+}
+
+// Validate makes sure we've built our struct properly. It is usually unused for
+// normal functions that users can use directly.
+func (obj *FileExistsFunc) Validate() error {
+	return nil
+}
+
+// Info returns some static info about itself.
+func (obj *FileExistsFunc) Info() *interfaces.Info {
+	return &interfaces.Info{
+		Pure: false, // maybe false because the file contents can change
+		Memo: false,
+		Fast: false,
+		Spec: false,
+		Sig:  types.NewType(fmt.Sprintf("func(%s str) bool", fileExistsArgNameFilename)),
+	}
+}
+
+// Init runs some startup code for this function.
+func (obj *FileExistsFunc) Init(init *interfaces.Init) error {
+	obj.init = init
+	if obj.data == nil {
+		// programming error
+		return fmt.Errorf("missing function data")
+	}
+	return nil
+}
+
+// Copy is implemented so that the obj.built value is not lost if we copy this
+// function.
+func (obj *FileExistsFunc) Copy() interfaces.Func {
+	return &FileExistsFunc{
+		Textarea: obj.Textarea,
+
+		init: obj.init, // likely gets overwritten anyways
+		data: obj.data, // needed because we don't call SetData twice
+	}
+}
+
+// Call this function with the input args and return the value if it is possible
+// to do so at this time.
+func (obj *FileExistsFunc) Call(ctx context.Context, args []types.Value) (types.Value, error) {
+	if len(args) < 1 {
+		return nil, fmt.Errorf("not enough args")
+	}
+	filename := args[0].Str()
+
+	// TODO: add validation for absolute path?
+	// TODO: add check for empty string
+
+	if obj.data == nil {
+		return nil, funcs.ErrCantSpeculate
+	}
+	p := strings.TrimSuffix(obj.data.Base, "/")
+	if p == obj.data.Base { // didn't trim, so we fail
+		// programming error
+		return nil, fmt.Errorf("no trailing slash on Base, got: `%s`", p)
+	}
+	path := p
+
+	if !strings.HasPrefix(filename, "/") {
+		return nil, fmt.Errorf("filename was not absolute, got: `%s`", filename)
+		//path += "/" // be forgiving ?
+	}
+	path += filename
+
+	if obj.init == nil || obj.data == nil {
+		return nil, funcs.ErrCantSpeculate
+	}
+	fs, err := obj.init.World.Fs(obj.data.FsURI) // open the remote file system
+	if err != nil {
+		return nil, errwrap.Wrapf(err, "can't load data from file system `%s`", obj.data.FsURI)
+	}
+	// this is relative to the module dir the func is in!
+	// stat file...
+	_, err = fs.Stat(filename)
+	if err == nil {
+		return &types.BoolValue{
+			V: true, // found
+		}, nil
+	}
+	if os.IsNotExist(err) {
+		return &types.BoolValue{
+			V: false, // not found
+		}, nil
+	}
+
+	if os.IsPermission(err) {
+		return nil, err // XXX: what should we do in this case?
+	}
+
+	// Some other real error...
+	return nil, err
+}

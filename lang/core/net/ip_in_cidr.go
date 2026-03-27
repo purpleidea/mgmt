6cd684163366764393428bf022a1686f9ad1b5e0
James Shubin <james@shubin.ca>
2026-03-27 16:59:41 -0400

    lang: core: net: Add a contains style function for ip in network

---
+++ lang/core/net/ip_in_cidr.go
@@ -0,0 +1,71 @@
+// Mgmt
+// Copyright (C) James Shubin and the project contributors
+// Written by James Shubin <james@shubin.ca> and the project contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Additional permission under GNU GPL version 3 section 7
+//
+// If you modify this program, or any covered work, by linking or combining it
+// with embedded mcl code and modules (and that the embedded mcl code and
+// modules which link with this program, contain a copy of their source code in
+// the authoritative form) containing parts covered by the terms of any other
+// license, the licensors of this program grant you additional permission to
+// convey the resulting work. Furthermore, the licensors of this program grant
+// the original author, James Shubin, additional permission to update this
+// additional permission if he deems it necessary to achieve the goals of this
+// additional permission.
+
+package corenet
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/purpleidea/mgmt/lang/funcs/simple"
+	"github.com/purpleidea/mgmt/lang/types"
+)
+
+func init() {
+	simple.ModuleRegister(ModuleName, "ip_in_cidr", &simple.Scaffold{
+		I: &simple.Info{
+			Pure: true,
+			Memo: true,
+			Fast: true,
+			Spec: true,
+		},
+		T: types.NewType("func(ip str, cidr str) bool"),
+		F: IpInCidr,
+	})
+}
+
+// IpInCidr returns true if the input ip is part of a CIDR network. If either
+// value is invalid this errors.
+func IpInCidr(ctx context.Context, input []types.Value) (types.Value, error) {
+	s := input[0].Str()
+	ip := net.ParseIP(s)
+	if ip == nil {
+		return nil, fmt.Errorf("invalid IP: %s", s)
+	}
+
+	_, network, err := net.ParseCIDR(input[1].Str())
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.BoolValue{
+		V: network.Contains(ip),
+	}, nil
+}
